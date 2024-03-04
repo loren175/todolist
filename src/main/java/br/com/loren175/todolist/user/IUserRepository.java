3574c5f815ddf422e2637b1010667f4559d06074
Rafael Loren175 <rafael.loren175@gmail.com>
2024-03-04 09:37:01 -0300

    first commit

---
+++ src/main/java/br/com/loren175/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.loren175.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  UserModel findByUsername(String username);
+}
